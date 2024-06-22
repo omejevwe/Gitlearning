e6668cb44a3d345cf85dc27ece20ca61685e7ad8
Omejevwe <omejevwe@Omejevwes-MacBook-Pro-2.local>
2024-06-22 15:40:55 +0100

    Added functions that multplies numbers

--- hello_world.py
+++ hello_world.py
@@ -1,5 +1,9 @@
 print("Welcome to python class")
 # function to add two number
-def addnumbers(a,b):
+def add_numbers(a,b):
     add = a+b
     print(add)
+
+def multiply_numbers(a,b):
+    multiply = a*b
+    print(multiply)
